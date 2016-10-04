cce6d1307f07fbf6313ad7f0f16ddad55e77519a
Sebastian Pech <spech@spech.de>
2016-10-04 21:31:13 +0200

    Layout

---
+++ resources/views/layout_footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      xxxxxxxxxxxxxxxxxxxxxxxxx
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="http://www.spech.de/">Sebastian Pech</a>.</strong> All rights reserved.
+  </footer>
