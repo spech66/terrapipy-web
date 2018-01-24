8289f167d4c53c1999bc8105885d052e337ea89b
Sebastian Pech <spech@spech.de>
2018-01-24 23:44:25 +0100

    Refactored actuator

--- actuator.php
+++ actuator.php
@@ -9,9 +9,20 @@ require_once("_header.php") ?>
 <?php
 $devices = getDevices($pimaticUsername, $pimaticPassword, $pimaticHost);
 
-usort($devices, function($a, $b) {
+usort($devices, function($b, $a) { // Reversed b/a so that the "ugly" devices are on the bottom
 	return strcmp($a->template, $b->template);
 });
+
+function RenderBox($color, $icon, $header, $unit, $device)
+{
+	echo '<div class="col-lg-3 col-xs-6">';
+		echo '  <div class="small-box bg-'.$color.'">';
+		echo '	<div class="inner"><h3>'.$header.'<sup style="font-size: 20px">'.$unit.'</sup></h3><p>'.$device->name.'</p></div>';
+		echo '	<div class="icon"><i class="ion ion-'.$icon.'"></i></div>';
+		echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
+		echo '  </div>';
+	echo '</div>';
+}
 
 	foreach($devices as $device)
 	{
@@ -19,72 +30,28 @@ usort($devices, function($a, $b) {
 
 		if($device->template === 'temperature') // Actuator or sensor only?
 		{
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-red">';
-			echo '	<div class="inner"><h3>'.round($device->attributes[0]->value, 1).'<sup style="font-size: 20px">'.$device->attributes[0]->unit.'</sup></h3><p>'.$device->name.'</p></div>';
-			echo '	<div class="icon"><i class="ion ion-thermometer"></i></div>';
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
+			RenderBox("red", "thermometer", round($device->attributes[0]->value, 1), $device->attributes[0]->unit, $device);
 
-			if(array_key_exists(1, $device->attributes)) {
-				echo '<div class="col-lg-3 col-xs-6">';
-				echo '  <div class="small-box bg-aqua">';
-				echo '	<div class="inner"><h3>'.round($device->attributes[1]->value, 1).'<sup style="font-size: 20px">'.$device->attributes[1]->unit.'</sup></h3><p>'.$device->name.'</p></div>';
-				echo '	<div class="icon"><i class="ion ion-waterdrop"></i></div>';
-				echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-				echo '  </div>';
-				echo '</div>';
+			if(array_key_exists(1, $device->attributes)) { // Humidity
+				RenderBox("aqua", "waterdrop", round($device->attributes[1]->value, 1), $device->attributes[1]->unit, $device);
 			}
 
 		} else if($device->template === 'inputTime') {
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-blue">';
-			echo '	<div class="inner"><h3>'.$device->attributes[0]->value.'</h3><p>'.$device->name.'</p></div>';
-			echo '	<div class="icon"><i class="ion ion-clock"></i></div>';
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
+			RenderBox("blue", "clock", $device->attributes[0]->value, "", $device);
 		} else if($device->template === 'switch') {
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-green">';
-			echo '	<div class="inner"><h3>'.$device->attributes[0]->labels[!$device->attributes[0]->value].'</h3><p>'.$device->name.'</p></div>';
-			if($device->attributes[0]->value == 1) {
-				echo '	<div class="icon"><i class="ion ion-toggle-filled"></i></div>';
-			} else {
-				echo '	<div class="icon"><i class="ion ion-toggle"></i></div>';
-			}
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
+			$icon = $device->attributes[0]->value == 1 ? "ion ion-toggle-filled" : "ion ion-toggle";
+			RenderBox("green", $icon, $device->attributes[0]->labels[!$device->attributes[0]->value], "", $device);
 		} else if($device->template === 'input') {
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-yellow">';
-			echo '	<div class="inner"><h3>'.$device->attributes[0]->value.'</h3><p>'.$device->name.'</p></div>';
-			echo '	<div class="icon"><i class="ion ion-compose"></i></div>';
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
+			RenderBox("yellow", "compose", $device->attributes[0]->value, "", $device);
 		} else if($device->template === 'device') {
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-aqua">';
+			//RenderBox("aqua", "flash", $device->attributes[0]->value, "", $device);
 			foreach($device->attributes as $attr)
 			{
-				echo '	<div class="inner"><h3>'.$attr->label.' = '.$attr->value.'</h3></div>';
+				//RenderBox("aqua", "flash", $attr->label.' = '.$attr->value, "", $device);
+				RenderBox("aqua", "flash", $attr->value, $attr->label, $device);
 			}
-			echo '  <div class="inner"><p>'.$device->name.'</p></div>';
-			echo '	<div class="icon"><i class="ion ion-flash"></i></div>';
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
 		} else {
-			echo '<div class="col-lg-3 col-xs-6">';
-			echo '  <div class="small-box bg-gray">';
-			echo '	<div class="inner"><h3>'.$device->attributes[0]->value.'</h3><p>'.$device->name.'</p></div>';
-			echo '	<div class="icon"><i class="ion ion-help-circled"></i></div>';
-			echo '	<a href="actuator.php?id='.$device->id.'" class="small-box-footer">Anschauen <i class="fa fa-arrow-circle-right"></i></a>';
-			echo '  </div>';
-			echo '</div>';
+			RenderBox("gray", "help-circled", $device->attributes[0]->value, "", $device);
 		}
 	}
 ?>
